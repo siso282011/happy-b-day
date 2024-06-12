46b3630ccbb3506485914d6d80d408e3132aaf46
siso282011 <siso0208@icloud.com>
2024-06-12 15:24:11 +0300

    script.js

--- script.js
+++ script.js
@@ -14,4 +14,16 @@ var x = setInterval(function() {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance %
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="timer"
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+        document.getElementById("birthdayMessage").style.display = "block";
+    }
+}, 1000);
